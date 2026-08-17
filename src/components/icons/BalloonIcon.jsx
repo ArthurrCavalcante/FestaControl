dff775b8f6bd08b8bcc4208368b4e3e906176412
Arthur Cavalcante <ag7006930@gmail.com>
2026-08-17 02:17:07 -0300

    feat: redesign BalloonIcon to be more elegant and professional

--- src/components/icons/BalloonIcon.jsx
+++ src/components/icons/BalloonIcon.jsx
@@ -14,10 +14,14 @@ const BalloonIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2, ...pr
       strokeLinejoin="round" 
       {...props}
     >
-      <path d="M12 20c-1.1 0-2-.9-2-2 0-3.3-3-4.7-3-8 0-4.4 3.6-8 8-8s8 3.6 8 8c0 3.3-3 4.7-3 8 0 1.1-.9 2-2 2z" />
-      <path d="M12 20v4" />
-      <path d="M10 20h4" />
-      <path d="M9.5 5.5a2.5 2.5 0 0 0-2 2" />
+      {/* Balão elegante */}
+      <path d="M12 2C8.134 2 5 5.134 5 9c0 4.5 4.5 9 7 11 2.5-2 7-6.5 7-11 0-3.866-3.134-7-7-7Z" />
+      {/* Reflexo / Brilho */}
+      <path d="M8.5 7A3.5 3.5 0 0 1 12 3.5" />
+      {/* Nó do balão */}
+      <path d="M10.5 20h3" />
+      {/* Corda com leve curvatura */}
+      <path d="M12 20c0 1-1.5 1.5-1.5 2.5S12 24 12 24" />
     </svg>
   );
 };
